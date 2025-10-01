021e2bec069403dea5bdaca06b054fd38fbf332a
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-10-01 06:48:30 +0600

    remove unused

--- app/(dashboard)/dashboard/super-admin/outbound/calender/page.tsx
+++ app/(dashboard)/dashboard/super-admin/outbound/calender/page.tsx
@@ -1,26 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// import { env } from "@/env";
-
-// export default function AdminCalendarPage() {
-//   return (
-//     <div className="flex justify-center items-center p-6 bg-white">
-//       <iframe
-//         src={`https://calendar.google.com/calendar/embed?src=${env.NEXT_PUBLIC_OUTBOUND_EMAIL}`}
-//         style={{
-//           border: "0",
-//           backgroundColor: "transparent",
-//         }}
-//         width="100%"
-//         height="100%"
-//         className="h-[calc(100vh-155px)] bg-dark3"
-//       ></iframe>
-//     </div>
-//   );
-// }
-
 "use client";
 
-import { useState } from "react";
 import { env } from "@/env";
 
 export default function AdminCalendarPage() {
