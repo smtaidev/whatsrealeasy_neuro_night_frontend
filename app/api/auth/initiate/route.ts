94632527bc28ce40a3ff6ae4f090249f91a758db
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-10-03 04:00:32 +0600

    fix appointment initiate

--- app/api/auth/initiate/route.ts
+++ app/api/auth/initiate/route.ts
@@ -1,43 +1,4 @@
-// import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {
-//     const res = await fetch(
-//       `${process.env.API_BASE_URL}/appointments/auth/initiate`
-//     );
-
-//     const data = await res.json();
-
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return NextResponse.json(
-//       { error: "Something went wrong" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   try {
-//     const res = await fetch(
-//       ${process.env.API_BASE_URL}/appointments/auth/initiate
-//     );
-
-//     const data = await res.json();
-
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return NextResponse.json(
-//       { error: "Something went wrong" },
-//       { status: 500 }
-//     );
-//   }
-// }
 
 import { NextResponse } from "next/server";
 import { getAccessToken } from "@/lib/getServerAuth";
